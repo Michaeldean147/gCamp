68b545199e2d2ac4f8c95a33208fd33092a6bd28
Michaeldean147 <michaeldean147@yahoo.com>
2014-12-04 10:12:27 -0800

    due date commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203232939) do
+ActiveRecord::Schema.define(version: 20141204180318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141203232939) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "completed",   default: false, null: false
+    t.date     "due_date"
   end
 
 end
