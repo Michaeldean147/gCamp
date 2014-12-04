9e3a1611f1e02c6ef4d305cd042aeffd88fdda9d
Michaeldean147 <michaeldean147@yahoo.com>
2014-12-04 14:19:24 -0800

    user edit commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,22 @@ class UsersController < ApplicationController
       end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
