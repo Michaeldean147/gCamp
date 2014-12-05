0a1109697f1cd700a9f488238eecafce2d1164ec
Michaeldean147 <michaeldean147@yahoo.com>
2014-12-05 14:23:57 -0800

    user show commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -27,7 +31,7 @@ class UsersController < ApplicationController
     @user.update(user_params)
 
     if @user.save
-      redirect_to users_path
+      redirect_to users_path, notice: "User was successfully updated."
     else
       render :edit
     end
