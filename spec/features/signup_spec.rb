03db146cb69684f69b2c77e9a80d65b0b17f92ae
Michaeldean147 <michaeldean147@yahoo.com>
2014-12-18 14:44:49 -0800

    added signup tests for valid and invalid entries

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'User signup' do
+  scenario 'user signs up with valid information' do
+
+    visit root_path
+    click_on 'Sign Up'
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Smith"
+    fill_in "Email", with: "smith999@osu.edu"
+    fill_in "Password", with: "buckeyes"
+    fill_in "Password confirmation", with: "buckeyes"
+
+    within('.well') do
+      click_on "Sign up"
+    end
+
+    expect(page).to have_content("John Smith")
+  end
+
+  scenario 'user signs up without required information' do
+
+    visit root_path
+    click_on 'Sign Up'
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Smith"
+    fill_in "Email", with: "smith999@osu.edu"
+
+    within('.well') do
+      click_on "Sign up"
+    end
+
+    expect(page).to have_content("Password can't be blank")
+  end
+end
